24849f50e38f202dd4bbe5265bebca209fda6379
MartinVacheron <martinvdu92@gmail.com>
2026-03-10 18:24:10 +0100

    fix: phantom iterator variable associated with interned name
    In current version, index 0 is associated to string `"value"`, meaning that inside a `for` loop it is impossible to declare a variable named `value`

--- src/core/analyzer/Analyzer.zig
+++ src/core/analyzer/Analyzer.zig
@@ -384,7 +384,7 @@ fn forLoop(self: *Self, node: *const Ast.For, ctx: *Context) StmtResult {
     const res = try self.analyzeExpr(node.expr, .value, ctx);
 
     // Virtual scope to declare the iterator
-    _ = try self.forwardDeclareVariable(0, undefined, false, undefined);
+    _ = try self.forwardDeclareVariable(undefined, undefined, false, undefined);
 
     const kind: Instr.For.Kind, const elem_type = switch (res.type.*) {
         .array => |t| .{ .array, t.child },
